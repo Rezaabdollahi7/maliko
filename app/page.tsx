4c9e4f12d9b6ad5783a83c1271178edb6ff57368
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-12-29 22:21:44 +0330

    feat(footer): add footer layout, social media icons, and contact information

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Cta from "./components/Cta";
+import Footer from "./components/Footer";
 import SocialProofs from "./sections/SocialProofs";
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <SocialProofs />
       <Cta />
+      <Footer />
     </div>
   );
 }
